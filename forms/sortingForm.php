15ab2b3cee804021582dea88fcebfd5a90ec5e80
dmytryG <dmytrygolub@gmail.com>
2022-06-04 23:02:24 +0300

    Sorting form

---
+++ forms/sortingForm.php
@@ -0,0 +1,18 @@
+<div class="centred contentBlock">
+    <div>
+        <form action="/myProjects.php" method="post">
+            <div class="lineral filter-form-fields">
+                <select name="filter" id="filter">
+                        <option value="newer">Спочатку нові</option>
+                        <option value="older">Спочатку старі</option>
+                        <option value="completed">Виконані</option>
+                        <option value="new">Нові</option>
+                        <option value="review">Потребують правок</option>
+                </select>
+                <input type="submit" value="Сортувати/фільтрувати">
+            </div>
+        </form>
+    </div>
+</div>
+
+
