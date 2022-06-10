c0dda21e1159fc00db28aede39ee50a72d9d9bba
dmytryG <dmytrygolub@gmail.com>
2022-06-11 01:50:01 +0300

    log out mechanics

--- login.php
+++ login.php
@@ -9,23 +9,33 @@
     $db = new MySQL();
     $blocks = array();
     $is_just_logged_id = false;
+    $is_unlogging = false;
     global $COOKIE_LOGIN_TIME;
 
-    if(isset($_GET["login_as"])) {
-        try{
+    if (isset($_GET["login_as"]) && !strcmp($_GET["login_as"], "noUser")) {
+        unset($_COOKIE['login']);
+        unset($_COOKIE['user_status']);
+        setcookie('login', null, -1, '/');
+        setcookie('user_status', null, -1, '/');
+        header("Refresh: 0; url=" . $_SERVER['PHP_SELF']);
+        $is_unlogging = true;
+    }
+
+    if(isset($_GET["login_as"]) && !$is_unlogging) {
+        try {
             $user = $db->login($_GET["login_as"], "");
             $user = $db->set_user_statuses(array($user))[0];
             setcookie("login", $user->getUsername(), time() + $COOKIE_LOGIN_TIME);
             setcookie("user_status", $user->getStatus(), time() + $COOKIE_LOGIN_TIME);
             $is_just_logged_id = true;
-            header("Refresh: 0; url=".$_SERVER['PHP_SELF']);
+            header("Refresh: 0; url=" . $_SERVER['PHP_SELF']);
         } catch (Exception $ex) {
             $blocks[] = new ContentBlock("Помилка",
                 "Нажаль, сталась помилка під час отримання користувачів системи, 
-                буль-ласка, спробуйте пізніше: " . $ex->getMessage());
+            буль-ласка, спробуйте пізніше: " . $ex);
         }
     }
-    $isLoggedIn = isset($_COOKIE["login"]) && isset($_COOKIE["user_status"]);
+    $isLoggedIn = isset($_COOKIE["login"]) && isset($_COOKIE["user_status"]) && !$is_unlogging;
 
     if ($isLoggedIn || $is_just_logged_id) {
         $blocks[] = new ContentBlock("Оберіть обліковий запис",
@@ -41,11 +51,15 @@
         $user_arr = $db->set_user_statuses($user_arr);
         foreach ($user_arr as $user) {
             $blocks[] = new CustomBlock(
-                "<a href=\"?login_as=".$user->getUsername()."\">
-                Увійти як " . $user->getUsername() . " зі статусом " . $user->getStatus()
+                "<a href=\"?login_as=" . $user->getUsername() . "\">
+            Увійти як " . $user->getUsername() . " зі статусом " . $user->getStatus()
                 . "</a>", "bigButton"
             );
         }
+        $blocks[] = new CustomBlock(
+            "<a href=\"?login_as=noUser\">
+            Вийти з системи </a>", "bigButton"
+        );
     } catch (Exception $e) {
         $blocks[] = new ContentBlock("Помилка",
             "Нажаль, сталась помилка під час отримання користувачів системи, буль-ласка, спробуйте пізніше");
